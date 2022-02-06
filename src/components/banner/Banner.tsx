b4bab2ef73576fe42e714bdb35de3be8b4b56051
Hannah Gann <hannahegann@gmail.com>
2022-02-06 10:18:19 -0800

    added banner and changed about

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,17 @@
+
+export function Banner() {
+    const linkStyle = {
+        "textDecoration": "underline"
+    }
+    return (
+      <div className="bg-pink-400">
+        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="flex items-center flex-wrap">
+            <p className="font-medium grow text-white text-center">
+            A Harry Styles inspired Wordle clone.
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,19 +9,31 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source clone of the game Wordle -{' '}
+        This is a Harry Styles inspired clone of the game Wordle adapted by{' '}   
+        <a
+          href="https://twitter.com/chasm2018"
+          className="underline font-bold"
+          target="_blank"
+        >
+           chasm2018
+        </a>{' '}
+      </p>
+      <br/>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
         <a
           href="https://github.com/hannahcode/wordle"
           className="underline font-bold"
+          target="_blank"
         >
-          check out the code here
+          view the oiginal code here
         </a>{' '}
         and{' '}
         <a
           href="https://www.powerlanguage.co.uk/wordle/"
           className="underline font-bold"
+          target="_blank"
         >
-          play the original here
+          play the original game here
         </a>
       </p>
     </BaseModal>
