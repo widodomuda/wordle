09dc5fa916909e98a82c003dbd31ce7f64015303
Hannah Gann <hannahegann@gmail.com>
2022-02-06 11:35:33 -0800

    changed info page to be harry specific

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,38 +10,38 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the WORDLE in 6 tries. After each guess, the color of the tiles
+        Guess the WORDLE in 6 tries. The final answer will relate to Harry. After each guess, the color of the tiles
         will change to show how close your guess was to the word.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="W" status="correct" />
-        <Cell value="E" />
+        <Cell value="H" status="correct" />
         <Cell value="A" />
+        <Cell value="R" />
         <Cell value="R" />
         <Cell value="Y" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter H is in the word and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
-        <Cell value="L" status="present" />
-        <Cell value="O" />
+        <Cell value="S" />
         <Cell value="T" />
+        <Cell value="Y" />
+        <Cell value="L" />
+        <Cell value="E" status="present" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter E is in the word but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
         <Cell value="A" />
-        <Cell value="G" />
+        <Cell value="L" />
+        <Cell value="B" />
         <Cell value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="M" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter U is not in the word in any spot.
